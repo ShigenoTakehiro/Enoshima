ce5d79aa3597f9baba061e329b1b69631c94305d
ShigenoTakehiro <91305787+ShigenoTakehiro@users.noreply.github.com>
2022-12-09 04:41:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-
+> window.onload = () => {
+>      let places = staticLoadPlaces();
+>      renderPlaces(places);
+> };
+> 
+> function staticLoadPlaces() {
+>     return [
+>         {
+>             name: 'demo',
+>             location: {
+>                 lat: 35.3838139,
+>                 lng: 139.4711693,
+>             }
+>         },
+>     ];
+> }
+> 
+> function renderPlaces(places) {
+>     let scene = document.querySelector('a-scene');
+> 
+>     places.forEach((place) => {
+>         let latitude = place.location.lat;
+>         let longitude = place.location.lng;
+> 
+>         let model = document.createElement('a-entity');
+>         model.setAttribute('gps-entity-place', `latitude: $35.3838139; longitude: $139.4711693;`);
+>         model.setAttribute('gltf-model', 'img/demo.gltf');
+>         model.setAttribute('rotation', '0 0 0');
+>         model.setAttribute('animation-mixer', '');
+>         model.setAttribute('scale', '0.5 0.5 0.5');
+> 
+>         model.addEventListener('loaded', () => {
+>             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+>         });
+> 
+>         scene.appendChild(model);
+>     });
+> }
